1b4db94d691891b99311380f283d912dade9a396
tpham61 <toan.chpham@gmail.com>
2019-05-19 14:03:10 -0700

    copying from original Toan_Animation

---
+++ Assets/World/Enemy_Spawn.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Spawn : MonoBehaviour
+{
+
+    public int max_enemy_num;
+    public int number_of_enemies;
+    public float camera_x_offset;
+    public float camera_y_offset;
+    public float spawn_rate_time;
+    public float spawn_rate;
+    public GameObject player;
+    int enemy_id;
+    public GameObject common_enemy_1; //Drones
+    public GameObject common_enemy_2; //Charger
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawn_rate_time = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Commons
+
+        spawn_rate_time += Time.deltaTime * 100;
+        if (number_of_enemies <= max_enemy_num)
+        {
+            if (spawn_rate_time > spawn_rate)
+            {
+                enemy_id = Random.Range(0, 2);
+                if (enemy_id == 0)
+                {
+                    Instantiate(common_enemy_1, new Vector2(player.transform.position.x + camera_x_offset,
+                        player.transform.position.y + camera_y_offset-10), Quaternion.identity);
+                }
+                else if (enemy_id == 1)
+                {
+                    Instantiate(common_enemy_2, new Vector2(player.transform.position.x + camera_x_offset+40,
+                        player.transform.position.y + camera_y_offset+30), Quaternion.identity);
+                }
+                number_of_enemies++;
+                spawn_rate_time = 0;
+            }
+        }
+    }
+}
---
+++ Assets/World/Firewall.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Firewall : MonoBehaviour {
+
+    public GameObject player;
+
+    public float speed;
+    public float speed_limit;
+    float normal_speed;
+    public float high_speed;
+    public float low_speed;
+    public float slow_speed_limit;
+    public float distance;
+    public float max_distance;
+    public float min_distance;
+
+    private Rigidbody2D rigidBody;
+    public Enemy_Spawn en_spawn;
+
+    // Use this for initialization
+    void Start() {
+        rigidBody = GetComponent<Rigidbody2D>();
+        normal_speed = speed;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Slow_Time playerScript = player.GetComponent<Slow_Time>();
+        distance = player.transform.position.x - transform.position.x;
+        if (rigidBody.velocity.x <= speed_limit)
+            rigidBody.AddForce(new Vector2(speed, 0), ForceMode2D.Impulse);
+        if (playerScript.is_time_slow)
+        {
+            speed = slow_speed_limit;
+        }
+        else
+        {
+            if (distance > max_distance)
+            {
+                speed = high_speed;
+            }
+            else if (distance < min_distance)
+            {
+                speed = low_speed;
+            }
+            else
+            {
+                speed = normal_speed;
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            //Debug.Log("Game Over");
+        }
+        if (collision.gameObject.tag == "Enemy" || collision.gameObject.tag == "Obstacles" ||
+            collision.gameObject.tag == "Banners" || collision.gameObject.tag == "Enemy_Bullet" ||
+             collision.gameObject.tag == "Charger_Jump_Point" || collision.gameObject.tag == "Player_Bullet")
+        {
+            if (collision.gameObject.tag == "Enemy")
+                en_spawn.number_of_enemies--;
+            Destroy(collision.gameObject);
+        }
+    }
+}
