7966aa2add057adc9ff0886b07551d94071748ee
XateAorora <GamerXD8@yahoo.com>
2019-05-19 12:57:16 -0700

    bullet change and organization

---
+++ CSC307_Runner/Assets/Sprites-Toan/BottomFileFill_Toan.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottomFileFill_Toan : MonoBehaviour
+{
+    public GameObject bottomfire;
+    public float x;
+    // Start is called before the first frame update
+    void Start()
+    {
+        while (x < 119f)
+        {
+            GameObject newFire = Instantiate(bottomfire, new Vector3(x + Random.Range(-0.5f, 0.5f), transform.position.y + Random.Range(0f, 1.5f),
+                transform.position.z), transform.rotation);
+            newFire.transform.parent = gameObject.transform;
+            x += 1.2f;
+        }
+    }
+}
---
+++ CSC307_Runner/Assets/Sprites-Toan/ExplosionAnimation_Toan.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionAnimation_Toan : MonoBehaviour
+{
+    public GameObject Wall_Explosion;
+
+
+    // Update is called once per frame
+    private void Start()
+    {
+        StartCoroutine(spawnExplosion());
+    }
+
+    IEnumerator spawnExplosion()
+    {
+        while (true) // while game not over
+        {
+            yield return new WaitForSeconds(0.3f);
+            Instantiate(Wall_Explosion, transform.position, transform.rotation);
+            Instantiate(Wall_Explosion, new Vector3(transform.position.x + 
+                Random.Range(0f,2f), transform.position.y + Random.Range(-2.5f,2.5f), transform.position.z), transform.rotation) ;
+        }
+        
+    }
+}
