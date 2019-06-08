56dec7d5162919abaf114f0efe34a6573b784910
FelixSitu <chsitu@calpoly.edu>
2019-06-08 15:17:20 -0700

    Unit tests additions

--- Assets/Tests/PlayModeExampleTests.cs
+++ Assets/Tests/PlayModeExampleTests.cs
@@ -7,6 +7,9 @@ public class PlayModeTests : MonoBehaviour
 {
 
     public Meteor_Prefab meteor_script;
+    public Jump_Prefab player_jump;
+    public Slow_Prefab player_slow;
+    public Shoot_Prefab player_shoot;
     // public Player_Prefab player_script; 
 
     [UnityTest]
@@ -134,24 +137,130 @@ public class PlayModeTests : MonoBehaviour
 
         Destroy(meteor);
     }
-/*
+    /*
+        [UnityTest]
+        public IEnumerator testMeteorHitPlayer()
+        {
+            GameObject player =
+                MonoBehaviour.Instantiate(Resources.Load<GameObject>
+                    ("Prefab/Player"), new Vector3(0, 0, 0), Quaternion.identity);
+
+            GameObject meteor =
+                MonoBehaviour.Instantiate(Resources.Load<GameObject>
+                    ("Prefab/Meteor"), new Vector3(0, 0, 0), Quaternion.identity);
+
+            meteor_script = meteor.GetComponent<Meteor_Prefab>();
+            player_script = player.GetComponent<Player_Prefab>();
+            yield return new WaitForSeconds(0.5f);
+
+            Assert.True(meteor_script.got_hit);
+            Assert.True(player.hp == 2);
+        }
+        */
+
     [UnityTest]
-    public IEnumerator testMeteorHitPlayer()
+    public IEnumerator DoubleJump()
     {
         GameObject player =
             MonoBehaviour.Instantiate(Resources.Load<GameObject>
-                ("Prefab/Player"), new Vector3(0, 0, 0), Quaternion.identity);
+                ("Prefab/Player_Jump"));
 
-        GameObject meteor =
-            MonoBehaviour.Instantiate(Resources.Load<GameObject>
-                ("Prefab/Meteor"), new Vector3(0, 0, 0), Quaternion.identity);
+        player_jump = player.GetComponent<Jump_Prefab>();
+        float current_height = player_jump.GetComponent<Rigidbody2D>().position.y;
+        Assert.True(player_jump.jump_force > 0);
+        Assert.True(player_jump.double_jump_force > 0);
 
-        meteor_script = meteor.GetComponent<Meteor_Prefab>();
-        player_script = player.GetComponent<Player_Prefab>();
+        float height0 = current_height;
+        float height1 = current_height;
+        float height2 = current_height;
+
+        //First Jump
+        player_jump.jump();
+        yield return new WaitForSeconds(0.2f);
+        height1 = player_jump.GetComponent<Rigidbody2D>().position.y;
+
+        //double Jump
+        if (player_jump.did_jump)
+        {
+            player_jump.double_jump();
+            yield return new WaitForSeconds(0.2f);
+            height2 = player_jump.GetComponent<Rigidbody2D>().position.y;
+        }
+
+        Debug.Log("Height 0: " + height0);
+        Debug.Log("Height 1: " + height1);
+        Debug.Log("Height 2: " + height2);
+
         yield return new WaitForSeconds(0.5f);
 
-        Assert.True(meteor_script.got_hit);
-        Assert.True(player.hp == 2);
+        Assert.True(height1 > height0);
+        Assert.True(height2 > height1);
+    }
+
+    [UnityTest]
+    public IEnumerator SlowTime()
+    {
+        //SceneManager.LoadScene(2);
+
+        GameObject player =
+            MonoBehaviour.Instantiate(Resources.Load<GameObject>
+                ("Prefab/Player_Stop"));
+
+        player_slow = player.GetComponent<Slow_Prefab>();
+        player_slow.lower_limit = 0.5f;
+        player_slow.upper_limit = 1f;
+        float tolerance = 0.2f;
+        float delta;
+
+        Assert.True(player_slow.time_slow_time == 1f);
+
+        yield return new WaitForSeconds(0.5f);
+
+        player_slow.slowDown();
+        yield return new WaitForSeconds(1f);
+        delta = Mathf.Abs(player_slow.time_slow_time - player_slow.lower_limit);
+        Debug.Log("Slowing: " + delta);
+        Assert.True(delta < tolerance);
+
+        yield return new WaitForSeconds(0.5f);
+
+        player_slow.fastUp();
+        yield return new WaitForSeconds(1f);
+        delta = Mathf.Abs(player_slow.time_slow_time - player_slow.upper_limit);
+        Debug.Log("Fasting: " + delta);
+        Assert.True(delta < tolerance);
+    }
+
+    [UnityTest]
+    public IEnumerator PlayerShoot()
+    {
+        GameObject player =
+           MonoBehaviour.Instantiate(Resources.Load<GameObject>
+               ("Prefab/Player_Shoot"));
+        player_shoot = player.GetComponent<Shoot_Prefab>();
+        player_shoot.bullet = Resources.Load<GameObject>("Prefab/Bullet");
+
+        GameObject bullet1 = null;
+        GameObject bullet2 = null;
+
+        bullet1 = player_shoot.createBullet();
+        bullet2 = player_shoot.createBullet();
+
+        yield return new WaitForSeconds(0.5f);
+
+        Assert.NotNull(bullet1);
+        Assert.NotNull(bullet2);
+        Assert.AreNotSame(bullet1, bullet2);
+    }
+
+    /*
+    [UnityTest]
+    public IEnumerator Framerate()
+    {
+        SceneManager.LoadScene(2);
+        yield return new WaitForSeconds(0.5f);
+        Debug.Log(Application.targetFrameRate);
+        Assert.True(Application.targetFrameRate > 0);
     }
     */
 }
