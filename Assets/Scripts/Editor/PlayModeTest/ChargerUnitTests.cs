4a92328471fe2781f7fff10325f79b876f48daa5
Arman Rafian <armanrafian@gmail.com>
2019-06-09 22:52:02 -0700

    refactored tests

---
+++ Assets/Scripts/Editor/PlayModeTest/ChargerUnitTests.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class ChargerUnitTests : MonoBehaviour
+{
+    [UnityTest]
+    public IEnumerator testChargerMovement()
+    {
+        GameObject charger =
+            MonoBehaviour.Instantiate(Resources.Load<GameObject>
+                ("Prefab/Charger"));
+
+        Assert.IsNotNull(charger);
+
+        float position1 = charger.transform.position.x;
+        yield return new WaitForSeconds(0.5f);
+        float position2 = charger.transform.position.x;
+
+        Assert.False(position1 == position2);
+        Destroy(charger);
+    }
+}
---
+++ Assets/Scripts/Editor/PlayModeTest/MeteorUnitTests.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class MeteorUnitTests : MonoBehaviour
+{
+    [UnityTest]
+    public IEnumerator testMeteorMovement()
+    {
+        GameObject meteor =
+            MonoBehaviour.Instantiate(Resources.Load<GameObject>
+                ("Prefab/Meteor"));
+
+        float distance1_x = meteor.transform.position.x;
+        yield return new WaitForSeconds(0.5f);
+        float distance2_x = meteor.transform.position.x;
+
+        Assert.True(distance1_x == distance2_x);
+
+        float distance1_y = meteor.transform.position.y;
+        yield return new WaitForSeconds(0.5f);
+        float distance2_y = meteor.transform.position.y;
+
+        Assert.True(distance1_y > distance2_y);
+
+        Destroy(meteor);
+    }
+}
